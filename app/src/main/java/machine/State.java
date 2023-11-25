9e6811e14e3cdd1a412850ac6a1e4542d5e80bfd
Dpbm <dpbm136@gmail.com>
2023-11-25 14:17:06 -0300

    adding first automaton test

--- app/src/main/java/machine/State.java
+++ app/src/main/java/machine/State.java
@@ -1,21 +1,50 @@
 package machine;
 
+import java.util.ArrayList;
+
 public class State {
     private String id;
     private boolean firstState = false;
     private boolean finalState = false;
-    private Transition[] transitions;
+    private ArrayList<Transition> transitions = new ArrayList<Transition>();
 
-    public State(String id, Transition[] transitions) {
+    public State(String id) {
+        this.id = id;
+    }
+
+    public State(String id, ArrayList<Transition> transitions) {
         this.id = id;
         this.transitions = transitions;
     }
 
-    public State(String id, Transition[] transitions, boolean firstState, boolean finalState) {
+    public State(String id, ArrayList<Transition> transitions, boolean firstState, boolean finalState) {
         this.id = id;
         this.transitions = transitions;
         this.firstState = firstState;
         this.finalState = finalState;
     }
 
+    public void addTransiton(Transition transition) {
+        transitions.add(transition);
+    }
+
+    public void setFinal() {
+        this.finalState = true;
+    }
+
+    public boolean isFinal() {
+        return this.finalState;
+    }
+
+    public void setFirst() {
+        this.firstState = true;
+    }
+
+    public boolean isFirst() {
+        return this.firstState;
+    }
+
+    public String getId() {
+        return this.id;
+    }
 }
